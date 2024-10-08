77ef789722c8fcfe2b0b03cf749e4469754c9b94
Ritika Sharma <24octritika@gmail.com>
2024-10-08 23:21:52 +0100

    Added footer

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+export default function Footer () {
+    return (
+    <footer id="footer" className="footer">
+        <a href="https://www.linkedin.com/in/ritika-sharma-trinity/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+        <a href="https://github.com/ritika24-s" target="_blank" rel="noopener noreferrer">GitHub</a>
+        <a href="mailto:sharmari@tcd.ie">Gmail</a>
+        <a href="tel:+353-894832042">Phone</a>
+    </footer>
+    )
+}
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { useTheme } from '../utils/ThemeContext';
 import '../styles/Layout.css'; // Create a separate CSS for layout styling
 import Navbar from './Navbar'; // Import Navbar component
+import Footer from './Footer';
 
 const Layout = ({ children }) => {
   const { isDarkTheme, toggleTheme } = useTheme();
@@ -11,12 +12,7 @@ const Layout = ({ children }) => {
     <div className={`layout ${isDarkTheme ? 'dark' : 'light'}`}>
       <Navbar />
       <main>{children}</main>
-      <footer id="footer" className="footer">
-        <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer">LinkedIn</a>
-        <a href="https://github.com" target="_blank" rel="noopener noreferrer">GitHub</a>
-        <a href="mailto:your.email@gmail.com">Gmail</a>
-        <a href="tel:+1234567890">Phone</a>
-      </footer>
+      <Footer></Footer>
     </div>
   );
 };
